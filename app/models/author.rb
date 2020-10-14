a937d97b8dc413e27f0cd4fab2cabf5f82557036
Justin_Webb <Webb.n.justin@gmail.com>
2020-10-14 13:38:15 -0600

    model validations w & w/o regex

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
+# Author
+#   is valid
+#   is invalid with no name
+#   is invalid with a short number
+#   is invalid when non-unique
 class Author < ActiveRecord::Base
+    validates(:name,  presence: true, uniqueness: true) 
+    validates(:phone_number, { length: {minimum: 10}})
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,28 @@
+# Post
+#   is valid
+#   is invalid without a title
+#   is invalid with too short content
+#   is invalid with a long summary
+#   is invalid with a category outside the choices
+#   is invalid if not clickbait
+
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+    validate :is_clickbait?
+  
+    CLICKBAIT_PATTERNS = [
+      /Won't Believe/,
+      /Secret/,
+      /Top [0-9]*/,
+      /Guess/
+    ]
+  
+    def is_clickbait?
+      if CLICKBAIT_PATTERNS.none? { |pat| pat.match title }
+        errors.add(:title, "must be clickbait")
+      end
+    end
 end
